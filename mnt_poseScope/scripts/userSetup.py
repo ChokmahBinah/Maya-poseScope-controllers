15313f5678a71d3cc12d526ef5bf80c8c6b81cf5
ChokmahBinah <33164632+ChokmahBinah@users.noreply.github.com>
2021-05-02 14:50:35 +0200

    Added userSetup.py
    userSetup file helps Maya to run loading provided plugins and create poseScope shelf.

---
+++ mnt_poseScope/scripts/userSetup.py
@@ -0,0 +1,22 @@
+import importlib
+import maya.cmds as cmds
+import mnt_poseScopeShelf
+
+importlib.reload(mnt_poseScopeShelf)
+
+
+cmds.loadPlugin('mnt_locator.py')
+cmds.loadPlugin('mnt_groupNode.py')
+cmds.loadPlugin('mnt_poseScope.py')
+
+
+# Runs our custom tools at the end of Maya startup
+cmds.evalDeferred('mnt_poseScopeShelf.MntCustomShelf(\'mnt_poseScope\')')
+# ________________________________________________
+
+# Adds custom shapes to cached playback evaluation
+cmds.evalDeferred('cmds.cacheEvaluator(newFilter= \'nodeTypes\', newFilterParam=\'types=+mntLocator\', newAction=\'enableEvaluationCache\')')
+cmds.evalDeferred('cmds.cacheEvaluator(newFilter=\'nodeTypes\', newFilterParam = \'types=+mnt_poseScope\' , newAction = \'enableEvaluationCache\')')
+cmds.evalDeferred('cmds.cacheEvaluator(newFilter=\'nodeTypes\', newFilterParam = \'types=+mnt_poseScope\' , newAction = \'enableVP2Cache\', newActionParam = \'useHardware=0\')')
+cmds.evalDeferred('cmds.cacheEvaluator(newFilter=\'nodeTypes\', newFilterParam = \'types=+mnt_poseScope\' , newAction = \'enableVP2Cache\', newActionParam = \'useHardware=1\')')
+# ________________________________________________
