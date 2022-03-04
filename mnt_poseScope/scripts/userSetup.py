0e4eb6092b7335fdde13b41b8cdc9c18eaa619c6
ChokmahBinah <floriandelarque@gmail.com>
2022-03-04 10:17:37 +0100

    Added : - mnt_toggleCommandLine.png - mnt_toggleShelf.png - mnt_poseTool button to ToolBox

--- mnt_poseScope/scripts/userSetup.py
+++ mnt_poseScope/scripts/userSetup.py
@@ -1,5 +1,6 @@
 import importlib
 import maya.cmds as cmds
+import maya.mel as mel
 import mnt_poseScopeShelf
 
 importlib.reload(mnt_poseScopeShelf)
@@ -21,7 +22,28 @@ cmds.evalDeferred('cmds.cacheEvaluator(newFilter= \'nodeTypes\', newFilterParam=
 cmds.evalDeferred('cmds.cacheEvaluator(newFilter= \'nodeTypes\', newFilterParam=\'types=+mnt_locator\', newAction = \'enableVP2Cache\', newActionParam = \'useHardware=0\')')
 cmds.evalDeferred('cmds.cacheEvaluator(newFilter= \'nodeTypes\', newFilterParam=\'types=+mnt_locator\', newAction = \'enableVP2Cache\', newActionParam = \'useHardware=1\')')
 
-cmds.evalDeferred('cmds.cacheEvaluator(newFilter=\'nodeTypes\', newFilterParam = \'types=+mnt_poseScope\' , newAction = \'enableEvaluationCache\')')
-cmds.evalDeferred('cmds.cacheEvaluator(newFilter=\'nodeTypes\', newFilterParam = \'types=+mnt_poseScope\' , newAction = \'enableVP2Cache\', newActionParam = \'useHardware=0\')')
-cmds.evalDeferred('cmds.cacheEvaluator(newFilter=\'nodeTypes\', newFilterParam = \'types=+mnt_poseScope\' , newAction = \'enableVP2Cache\', newActionParam = \'useHardware=1\')')
+#cmds.evalDeferred('cmds.cacheEvaluator(newFilter=\'nodeTypes\', newFilterParam = \'types=+mnt_poseScope\' , newAction = \'enableEvaluationCache\')')
+#cmds.evalDeferred('cmds.cacheEvaluator(newFilter=\'nodeTypes\', newFilterParam = \'types=+mnt_poseScope\' , newAction = \'enableVP2Cache\', newActionParam = \'useHardware=0\')')
+#cmds.evalDeferred('cmds.cacheEvaluator(newFilter=\'nodeTypes\', newFilterParam = \'types=+mnt_poseScope\' , newAction = \'enableVP2Cache\', newActionParam = \'useHardware=1\')')
 # ________________________________________________
+
+# Adds custom buttons to ToolBox
+def addCustomToolBoxButtons():
+    gToolBox = mel.eval('$tmpVar=$gToolBox')
+    iconPath = cmds.getModulePath(moduleName = 'mnt_framework') + '/icons/mntShelf/'
+
+    if cmds.symbolCheckBox('mnt_poseTool', q = True, ex =True) == True:
+        cmds.deleteUI('mnt_poseTool')
+    cmds.symbolCheckBox('mnt_poseTool', i = iconPath + 'mnt_poseTool.png', w = 36, h = 36, p = gToolBox,\
+        onCommand = 'import mnt_poseTool as mnt_poseTool\nimportlib.reload(mnt_poseTool)\nposeTool = mnt_poseTool.mnt_DynSelection()', offCommand = 'poseTool.__del__()')
+
+    if cmds.symbolButton('toggleShelf', q = True, ex =True) == True:
+        cmds.deleteUI('toggleShelf')
+    cmds.symbolButton('toggleShelf', i = iconPath + 'mnt_toggleShelf.png', w = 36, h = 12, p = gToolBox, c = 'mel.eval(\'ToggleShelf;\')')
+
+    if cmds.symbolButton('toggleCommandLine', q = True, ex =True) == True:
+        cmds.deleteUI('toggleCommandLine')
+    cmds.symbolButton('toggleCommandLine', i = iconPath + 'mnt_toggleCommandLine.png', w = 36, h = 12, p = gToolBox, c = 'mel.eval(\'ToggleCommandLine;\')')
+
+cmds.evalDeferred('addCustomToolBoxButtons()')
+# ______________________________
